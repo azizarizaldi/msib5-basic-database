c3db3a2493d5bb20b3a0d89d53c1c1818abbe9ca
Aziz Arif Rizaldi <azizarifrizaldi@gmail.com>
2023-09-01 17:26:54 +0700

    Update readme + tambah dummy db

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # msib5-basic-database
 Repo ini digunakan untuk latihan basic database (Penggunaan Trigger, View, Function &amp; Stored Procedure)
+
+## Pengenalan
+Pada repo ini terdapat satu file sql [(example_database.sql)](example_database.sql) yang berisikan:
+- tabel rents
+- tabel rooms
+- tabel users
+
+Selain itu, terdapat trigger, view, function &amp; Stored Procedure yang telah saya buat.
+Untuk contoh pembuatannya, akan saya berikan contoh dibawah ini:
+
+### Pembuatan Trigger
+Silahkan buat database terlebih dahulu atau import file database yang ada di repo ini [(example_library.sql)](example_library.sql), lalu copy/paste query dibawah ini untuk membuat trigger:
+
+**Query dibawah ini bertujuan untuk membuat trigger penambahan data**
+```bash
+DELIMITER &&
+
+CREATE TRIGGER update_rooms_rented_count
+AFTER INSERT ON rents
+FOR EACH ROW
+BEGIN
+    UPDATE rooms
+    SET rented_count = rented_count + 1
+    WHERE id = NEW.room_id;
+END;
+
+&&
+
+DELIMITER ;
+```
+
+**Query dibawah ini bertujuan untuk membuat trigger pengurangan data**
+```bash
+DELIMITER &&
+
+CREATE TRIGGER decrease_rooms_rented_count
+AFTER DELETE ON rents
+FOR EACH ROW
+BEGIN
+    UPDATE rooms
+    SET rented_count = rented_count - 1
+    WHERE id = OLD.room_id;
+END;
+
+&&
+
+DELIMITER ;
+```
---
+++ example_library.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 31, 2023 at 10:02 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `example_library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rents`
+--
+
+CREATE TABLE `rents` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `room_id` int NOT NULL,
+  `duration` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `rented_count` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `address` text COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rents`
+--
+ALTER TABLE `rents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `rents`
+--
+ALTER TABLE `rents`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
